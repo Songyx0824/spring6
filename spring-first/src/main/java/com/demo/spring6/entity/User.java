878e61f66866cb713c86317319e45999cd94fcf6
Songyx0824 <18045025768@163.com>
2024-07-04 19:59:58 +0800

    111

---
+++ spring-first/src/main/java/com/demo/spring6/entity/User.java
@@ -0,0 +1,12 @@
+package com.demo.spring6.entity;
+
+public class User {
+
+    public User() {
+        System.out.println("无参构造器");
+    }
+
+    public void add(){
+        System.out.println("add.....");
+    }
+}
